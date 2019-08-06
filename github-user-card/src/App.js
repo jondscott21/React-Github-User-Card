0f8d2a21e8f8b2a4cf4b4b40e31a9d9a0653b55a
Jonathan Scott <jonathan.scott@gmail.com>
2019-08-06 12:14:00 -0700

    step 1 finished

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      users: []
+    }
+  }
+  componentDidMount() {
+    this.fetchUsers()
+  }
+  fetchUsers = () => {
+    fetch(`https://api.github.com/users/jondscott21`)
+    .then(res => {
+      return res.json();
+    })
+    .then(user => {
+      this.setState({
+        users: user
+      })
+    })
+  }
+  render() {
+    console.log(this.state.users)
+    return (
+      <div className="App">
+        <h1>test</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
